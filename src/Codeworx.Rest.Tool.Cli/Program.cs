1485b62541ae6d56a335d5a15443be617e07b978
Raphael Schwarz <raphael@codeworx.at>
2019-10-11 13:00:42 +0200

    Create netcoreapp2.1 wrapper Cli tool for net472 roslyn tool

---
+++ src/Codeworx.Rest.Tool.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Codeworx.Rest.Tool.Cli
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var location = new FileInfo(typeof(Program).Assembly.Location);
+            var path = Path.Combine(location.DirectoryName, "Codeworx.Rest.Tool.exe");
+            var process = Process.Start(path, string.Join(" ", args.Select(p => $"\"{p}\"")));
+            process.WaitForExit();
+        }
+    }
+}
