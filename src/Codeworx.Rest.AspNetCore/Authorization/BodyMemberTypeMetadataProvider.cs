23a8b84b248c8d8e06497a5fe467a678e200f2d7
Stefanie Breuer <stefanie.breuer@insite-it.net>
2022-10-21 09:13:23 +0200

    create a suggestion for a consumes attribute

---
+++ src/Codeworx.Rest.AspNetCore/Authorization/BodyMemberTypeMetadataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Codeworx.Rest.AspNetCore.Authorization
+{
+    public class BodyMemberTypeMetadataProvider : AttributeMetadataProvider<BodyMemberTypeAttribute>
+    {
+        protected override void TransformAction(BodyMemberTypeAttribute source, ActionModel model, MetadataProviderContext context)
+        {
+            var consumesAttribute = new ConsumesAttribute(source.ContentType, source.AdditionalContentTypes.ToArray());
+
+            model.Filters.Add(consumesAttribute);
+
+            var selectorModel = model.Selectors.First();
+            selectorModel.ActionConstraints.Add(consumesAttribute);
+            selectorModel.EndpointMetadata.Add(consumesAttribute);
+        }
+
+        protected override void TransformController(BodyMemberTypeAttribute source, ControllerModel model, MetadataProviderContext context)
+        {
+            // do nothing
+        }
+
+        protected override void TransformParameter(BodyMemberTypeAttribute source, ParameterModel model, MetadataProviderContext context)
+        {
+            // do nothing
+        }
+    }
+}
---
+++ src/Codeworx.Rest.Primitives/BodyMemberTypeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codeworx.Rest
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class BodyMemberTypeAttribute : Attribute
+    {
+        public BodyMemberTypeAttribute(string contentType, params string[] additionalContentTypes)
+        {
+            ContentType = contentType;
+            AdditionalContentTypes = additionalContentTypes;
+        }
+
+        public string ContentType { get; }
+
+        public IReadOnlyCollection<string> AdditionalContentTypes { get; }
+    }
+}
